6e0903a81ba3cf612724515318a5110425a00b36
Emon Dhar <tomasdhar7@gmail.com>
2026-08-09 03:25:53 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: [
+        "Web App Developer",
+        "UI/UX Designer",
+        "Mobile App Developer",
+        "AI & ML Enthusiast",
+        "Digital Marketer"
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1200,
+    loop: true
+});
